abcff8e7ca0e8e1763a8695bbabedb539c9a7bcb
Aaron Saray <me@aaronsaray.com>
2019-11-18 18:14:23 -0600

    image stuff is looking good

--- src/components/MainImage.vue
+++ src/components/MainImage.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <div v-if="!imageSource" id="uploader">
+    <div
+      v-if="!imageSource"
+      id="uploader"
+      :class="{ hovering: hovering }"
+      ref="uploader"
+    >
       <h3 class="text-center mb-3">Pick Your Image</h3>
       <form @submit.prevent="loadImageFromUrl()">
         <div class="input-group mb-3">
@@ -36,7 +41,7 @@
             type="file"
             class="custom-file-input"
             id="upload-file"
-            accept="image/gif, image/jpeg, image/png"
+            accept="image/*"
             @change="loadImageFromFile"
           />
           <label class="custom-file-label" for="upload-file">Choose file</label>
@@ -45,7 +50,7 @@
       <div class="text-center mb-3 text-muted">
         - or -
       </div>
-      <div class="text-center">
+      <div class="text-center" :class="{ 'font-weight-bold': hovering }">
         Drop Your Image Here
       </div>
     </div>
@@ -58,20 +63,49 @@
 <script>
 export default {
   name: "MainImage",
+
   props: {
     msg: String
   },
+
   data: function() {
     return {
       imageSource: null,
       url: null,
-      loadingUrl: false
+      loadingUrl: false,
+      hovering: false
     };
   },
 
+  mounted() {
+    let uploader = this.$refs.uploader;
+    uploader.addEventListener("dragenter", this.doHovering, false);
+    uploader.addEventListener("dragover", this.doHovering, false);
+    uploader.addEventListener("dragleave", this.stopHovering, false);
+    uploader.addEventListener("drop", this.stopHovering, false);
+    ["dragenter", "dragover", "dragleave", "drop"].forEach(eventName => {
+      uploader.addEventListener(
+        eventName,
+        e => {
+          e.preventDefault();
+          e.stopPropagation();
+        },
+        false
+      );
+    });
+    uploader.addEventListener("drop", this.loadImageFromDrop, false);
+  },
+
   methods: {
+    loadImageFromDrop(event) {
+      this.loadImageFromFileElement(event.dataTransfer.files[0]);
+    },
+
     loadImageFromFile(event) {
-      const file = event.target.files[0];
+      this.loadImageFromFileElement(event.target.files[0]);
+    },
+
+    loadImageFromFileElement(file) {
       const reader = new FileReader();
       reader.onload = e => {
         this.imageSource = e.target.result;
@@ -104,6 +138,14 @@ export default {
 
     emitHasImage() {
       this.$emit("has-image", true);
+    },
+
+    doHovering() {
+      this.hovering = true;
+    },
+
+    stopHovering() {
+      this.hovering = false;
     }
   }
 };
@@ -115,13 +157,44 @@ export default {
   margin: 1rem;
   padding: 1rem;
   box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  border: 2px dashed transparent;
+  &.hovering {
+    border-color: rgba(0, 0, 0, 0.5);
+    .input-group,
+    .text-muted {
+      opacity: 0.3;
+    }
+  }
 }
 #main-image {
-  background: #aaaaaa;
-  padding: 1rem;
   img {
+    display: block;
+    margin: auto;
     max-width: 100%;
     height: auto;
+    padding: 10px;
+
+    background-color: #fefefe;
+    background-image: linear-gradient(
+        45deg,
+        #cbcbcb 25%,
+        transparent 25%,
+        transparent 75%,
+        #cbcbcb 75%,
+        #cbcbcb
+      ),
+      linear-gradient(
+        45deg,
+        #cbcbcb 25%,
+        transparent 25%,
+        transparent 75%,
+        #cbcbcb 75%,
+        #cbcbcb
+      );
+    -webkit-background-size: 30px 30px;
+    -moz-background-size: 30px 30px;
+    background-size: 30px 30px;
+    background-position: 0 0, 15px 15px;
   }
 }
 </style>
